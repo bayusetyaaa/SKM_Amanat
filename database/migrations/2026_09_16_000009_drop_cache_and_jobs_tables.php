f27634fcc0662e3b6da263a3f09c8aa33398009b
bayusetyaaa <bayusetya123456789@gmail.com>
2026-09-16 19:50:31 +0700

    Drop unused cache and jobs tables and set default cache store to file

---
+++ database/migrations/2026_09_16_000009_drop_cache_and_jobs_tables.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('job_batches');
+        Schema::dropIfExists('jobs');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
